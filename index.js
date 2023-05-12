c1a82b3f03debca16c401fd36e1ad3472c68b5a2
Luiz Augusto Scarsi <116020373+LuizScarsi@users.noreply.github.com>
2023-05-12 15:02:07 -0300

    Add functions, limits one operation

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
 const digits = Array.from(document.querySelectorAll('.digit'));
 const display = document.querySelector('.display');
-const functions = Array.from(document.querySelectorAll('.function'));
+const operation = [];
 
-digits.forEach(digit => digit.addEventListener('click', () => {
+digits.forEach(digit => digit.addEventListener('click', ()=>{
     if (digit.textContent == 0 && display.textContent == '') return 0;
     if (digit.textContent == '.'){
         if (display.textContent == ''){
-            display.textContent = '0.'
+            operation.push('0','.')
+            display.textContent = '0.';
             return 0;
-        } else if(display.textContent.split('').includes('.')) {
+        } else if(operation.includes('.')) {
             return 0;
         }
     }
+    operation.push(digit.textContent);
     display.textContent += digit.textContent;
+    console.log(operation)
+}));
 
-}))
+const functions = Array.from(document.querySelectorAll('.function'));
+
+functions.forEach(func => func.addEventListener('click',()=>{
+    switch(func.id){
+        case 'clr':
+            operation.splice(0, operation.length)
+            display.textContent = '';
+            break;
+        case 'back':
+            operation.pop();
+            display.textContent = display.textContent.slice(0,-1);
+            break;
+    };
+    if (operation.includes('+')||operation.includes('-')||operation.includes('*')||operation.includes('/')){
+        return 0;
+    };
+    switch(func.id){
+        case '+':
+            operation.push(func.id);
+            display.textContent += func.id;
+            break;
+        case '-':
+            operation.push(func.id);
+            display.textContent += func.id;
+            break;
+        case '*':
+            operation.push(func.id);
+            display.textContent += func.id;
+            break;
+    };
+}));
 function operate(operator, firstNumber, secondNumber = null){
     switch(operator){
         case '+':
